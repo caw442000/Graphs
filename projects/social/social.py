af146ab062d9f5be5b3a7bf02212e3c1925ab1d6
cedric-winbush <cawinbushjr@gmail.com>
2020-07-15 18:50:37 -0500

    adds social completion

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,3 +1,7 @@
+import random
+
+from util import Queue
+
 class User:
     def __init__(self, name):
         self.name = name
@@ -7,7 +11,7 @@ class SocialGraph:
         self.last_id = 0
         self.users = {}
         self.friendships = {}
-
+        
     def add_friendship(self, user_id, friend_id):
         """
         Creates a bi-directional friendship
@@ -27,15 +31,20 @@ class SocialGraph:
         self.last_id += 1  # automatically increment the ID to assign the new user
         self.users[self.last_id] = User(name)
         self.friendships[self.last_id] = set()
+        
+    def fisher_yates_shuffle(self, l):
+        for i in range(0, len(l)):
+            random_index = random.randint(i, len(l) - 1)
+            l[random_index], l[i] = l[i], l[random_index]
 
     def populate_graph(self, num_users, avg_friendships):
         """
         Takes a number of users and an average number of friendships
         as arguments
-
+​
         Creates that number of users and a randomly distributed friendships
         between those users.
-
+​
         The number of users must be greater than the average number of friendships.
         """
         # Reset graph
@@ -43,24 +52,71 @@ class SocialGraph:
         self.users = {}
         self.friendships = {}
         # !!!! IMPLEMENT ME
-
+        # ​
         # Add users
+        ## use num_users
+        for user in range(num_users):
+            self.add_user(user)
 
         # Create friendships
+        ## make a list with all POSSIBLE friendships
+        ### Example:
+        # 5 users
+        # [(1, 2), (1, 3), (1, 4), (1, 5), (2, 3), (2, 4), (2, 5), (3, 4), (3, 5), (4, 5)]
+        friendships = []
+        for user in range(1, self.last_id + 1):
+            for friend in range(user + 1, num_users + 1):
+                friendship = (user, friend)
+                friendships.append(friendship)
+              
+        ## Shuffle the list
+        self.fisher_yates_shuffle(friendships)
+        
+        ## Take as many as we need
+        total_friendships = num_users * avg_friendships
 
+        random_friendships = friendships[:total_friendships//2]
+        ## add to self.friendships
+        for friendship in random_friendships:
+            self.add_friendship(friendship[0], friendship[1])
+        
+        
     def get_all_social_paths(self, user_id):
         """
         Takes a user's user_id as an argument
-
+​
         Returns a dictionary containing every user in that user's
         extended network with the shortest friendship path between them.
-
+​
         The key is the friend's ID and the value is the path.
         """
         visited = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
-        return visited
 
+        q = Queue()
+        # starting point
+        path = [user_id]
+        # put in queue
+        q.enqueue(path)
+
+        while q.size() > 0:
+            # pull off from queue
+            current_path = q.dequeue()
+            # look at the last element in the path
+            current_node = current_path[-1]
+            # check if it has been visited
+            if current_node not in visited:
+                # add to the visited dictionary with the key being the user and the value being the path
+                # this remembers the path so far
+                visited[current_node] = current_path
+                for neighbor in self.friendships[current_node]:
+
+                    path_copy = list(current_path)
+                    if neighbor not in current_path:
+                        path_copy.append(neighbor)
+                    q.enqueue(path_copy)
+        return visited
+        
 
 if __name__ == '__main__':
     sg = SocialGraph()
